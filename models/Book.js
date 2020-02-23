f6715310df9610ec30fdd0b8557ce60d894e4292
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-23 00:27:06 -0800

    created Book schema

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = Book = mongoose.model("Book", userSchema);
