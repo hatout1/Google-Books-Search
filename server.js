633af48b14821fcc4f22f8946ed93586fef72697
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-23 00:20:25 -0800

    added mongoose pck and made a connetion to DB

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3003;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.mongodb_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log("Error: ", err));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
