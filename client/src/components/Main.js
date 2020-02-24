5b0ace0bc3a879616259a2ff2256d54adce4a48c
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-24 08:37:02 -0800

    configured the file to run back-end and front-end concurrently

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import Header from "./Header/Header";
 import Book from "./Book/Book";
+import axios from "axios";
 
 function Main() {
     const [link, modifyLink] = useState({
         state: "search"
+    });
+
+    useEffect(() => {
+        axios
+            .get("/api/all")
+            .then(res => console.log(res))
+            .catch();
     });
 
     return (
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 require("dotenv").config();
 
