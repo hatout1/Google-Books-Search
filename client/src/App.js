b70014ca19ac02ea2d9f90b8dda762cd52aa9806
Adnan Niaz <adnanniaz77@yahoo.com>
2020-03-17 01:59:20 -0700

    api is funtional now

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,57 @@
-import React from "react";
-import Main from "./components/Main";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+    // const [appState, setState] = useState("search");
+    const [apiResult, setApiResult] = useState([]);
+    const [book, setBook] = useState("javascript");
+    const [apiKey, setApiKey] = useState(
+        "AIzaSyAakxYQit1DRIQag9AXDESHnUc2P6lNwBk"
+    );
+
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${book}&filter=free-ebooks&key=${apiKey}`;
+
+    const handleChange = e => {
+        const book = e.target.value;
+        setBook(book);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .get(apiUrl)
+            .then(res => {
+                console.log(res.data.items);
+                setApiResult(res.data.items);
+            })
+            .catch(err => console.error(err));
+    };
+
     return (
         <div className="App">
-            <Main />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>REACT Google Books</h1>
+                </div>
+                <input
+                    onChange={handleChange}
+                    className="form-control"
+                    type="text"
+                />
+                <button onClick={handleSubmit}>submit</button>
+                <ul>
+                    {apiResult.map(book => (
+                        <li key={book.id}>
+                            <a href={book.volumeInfo.previewLink}>
+                                <img
+                                    src={book.volumeInfo.imageLinks.thumbnail}
+                                    alt=""
+                                />
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
